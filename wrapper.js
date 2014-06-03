1faa54cba07bdf25d4fb1573a14e000d2d34f495
Peter Krautzberger <p.krautzberger@gmail.com>
2014-06-03 21:13:16 +0200

    first attempt at isolating svg clean up

--- wrapper.js
+++ wrapper.js
@@ -27,6 +27,22 @@ var inputFile = process.argv[2];
 //console.log(inputFile);
 var outputFile = process.argv[3];
 
+// function for moving paths to a global element
+function svgCleaning (localElement,globalElement) {
+    var thePaths = localElement.querySelectorAll("path");
+    for (var i = 0; i < thePaths.length; i++) {
+        var currentPath = thePaths[i];
+        var currentPathID = currentPath.getAttribute("id");
+        if ( globalElement.querySelector("#"+currentPathID) === null){
+            globalElement.firstChild.appendChild(currentPath);     
+        }
+        else {
+            currentPath.parentNode.removeChild(currentPath);
+//            console.log(currentPath);
+        }
+    }
+}
+
 //
 //  Process an HTML file:
 //    Find all math elements,
@@ -39,6 +55,10 @@ var outputFile = process.argv[3];
 function processHTML(html, callback) {
     var document = jsdom(html);
     var math = document.querySelectorAll('[type="math/tex"], [type="math/tex; mode=display"]');
+//    Creating a global SVG object collecting up all paths TODO: 
+    var globalSVG = document.createElement("svg");
+    globalSVG.setAttribute("style","visibility: hidden; overflow: hidden; position: absolute; top: 0px; height: 1px; width: auto; padding: 0px; border: 0px; margin: 0px; text-align: left; text-indent: 0px; text-transform: none; line-height: normal; letter-spacing: normal; word-spacing: normal;");
+    globalSVG.innerHTML = "<defs></defs>";
     for (var i = 0, m = math.length; i < m; i++) {
         var data = {
             math: math[i].text,
@@ -47,40 +67,29 @@ function processHTML(html, callback) {
         };
         typeset(data, (function (node, last) {
             return function (result) {
-                //        console.log(node.getAttribute("type"));
                 if (result.svg) {
-                    //          console.log(node.getAttribute("type"));
                     if (node.getAttribute("type") === 'math/tex; mode=display') {
                         var div = document.createElement("div");
                         div.innerHTML = result.svg;
                         var thisSVG = div.firstChild;
                         div.setAttribute("style", "text-align: center;");
-                        thisSVG.removeAttribute("style"); // the absolute positioning led to some problems
+                        thisSVG.removeAttribute("style"); // the absolute positioning led to some problems?
                         node.parentNode.replaceChild(div, node);
+                        svgCleaning(div,globalSVG);
                     }
                     else{ 
                         var span = document.createElement("span");
                         span.innerHTML = result.svg;
                         node.parentNode.replaceChild(span.firstChild, node);
+                        svgCleaning(span,globalSVG);
                         }
+                    
                 }
                 if (last) {
-                    var globalSVG = document.createElement("svg");
-                    globalSVG.setAttribute("style","visibility: hidden; overflow: hidden; position: absolute; top: 0px; height: 1px; width: auto; padding: 0px; border: 0px; margin: 0px; text-align: left; text-indent: 0px; text-transform: none; line-height: normal; letter-spacing: normal; word-spacing: normal;");
-                    globalSVG.innerHTML = "<defs></defs>";
-                    var allPaths = document.getElementsByTagName("path");
-                    for (var i = 0; i < allPaths.length; i++) {
-                        var currentPathID = allPaths[i].getAttribute("id");
-//                        console.log(currentPathID);
-//                        console.log();
-                        if ( globalSVG.querySelector("#"+currentPathID) === null){
-//                            console.log("test");
-                            globalSVG.firstChild.appendChild(allPaths[i]);     
-                        }
-                        else {
-                        allPaths[i].parentNode.removeChild(allPaths[i]);
-                        }
-                    }
+                    document.body.querySelectorAll("path").forEach(function(element){
+//                        console.log(element);
+                        element.parentNode.removeChild(element);
+                    });                                     
                     document.body.appendChild(globalSVG);
                     callback(document.outerHTML);
                 }
